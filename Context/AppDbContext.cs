c418d60168bd14707f68ada3f0ee0c4b88f36fa5
samuelhurtado20 <samuelhurtado20@hotmail.com>
2023-01-20 08:29:06 -0500

    Add project files.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD_React_NET_Core_Back.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_React_NET_Core_Back.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Managers> Managers { get; set; }
+    }
+}
---
+++ Models/Managers.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_React_NET_Core_Back.Models
+{
+    public class Managers
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Release { get; set; }
+        public string Developer { get; set; }
+    }
+}
